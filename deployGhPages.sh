35c12772bdf1169241e54ba86290099c52b9b116
Silvio Wangler <silvio.wangler@gmail.com>
2017-05-18 17:43:18 +0200

    Deploy Script

--- deployGhPages.sh
+++ deployGhPages.sh
@@ -1,1 +1,26 @@
 #!/usr/bin/env bash
+
+./gradlew clean asciidoctor
+
+BASEDIR=presentation/build
+SOURCEDIR="$BASEDIR/asciidoc/revealjs"
+WORKINGDIR="$BASEDIR/gh-pages"
+
+mkdir -p $WORKINGDIR
+cd $WORKINGDIR
+
+git clone git@github.com:saw303/spock-intro.git
+
+git checkout gh-pages
+
+git rm -rf .
+
+cp -r $SOURCEDIR/* $WORKINGDIR
+
+git add .
+
+git commit -m "Updated presentation"
+git push
+
+cd -
+rm -rf $WORKINGDIR
